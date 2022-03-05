4ed7f35017bf9303140d266f6299517925efc2b3
Cdeer <kwangrok92@naver.com>
2022-03-05 09:15:09 +0900

    before web view kit

---
+++ CustomPopupTuto/CustomPopupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPopupViewController.swift
+//  CustomPopupTuto
+//
+//  Created by 김광록 on 2022/03/04.
+//
+
+import UIKit
+
+class CustomPopupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("CustomPopupViewController Called")
+    }
+}
--- CustomPopupTuto/ViewController.swift
+++ CustomPopupTuto/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var CreatePopupBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onCreatePopupBtnClicked(_ sender: UIButton) {
+        
+        print("ViewController - onCreatePopupBtnClicked is played!")
+        
+        // 스토리보드 가져오기
+        let storyboard = UIStoryboard.init(name: "Popup", bundle: nil)
+        
+        // 스토리보드를 통해 뷰컨트롤러 가져오기
+        let alertPopupVC = storyboard.instantiateViewController(withIdentifier: "AlertPopupVC")
+        
+        // 뷰컨트롤러가 보여지는 스타일
+        alertPopupVC.modalPresentationStyle = .overCurrentContext
+        // 뷰컨트롤러가 사라지는 스타일
+        alertPopupVC.modalTransitionStyle = .crossDissolve
+        
+        self.present(alertPopupVC, animated: true, completion: nil)
+        
+    }
 }
 
